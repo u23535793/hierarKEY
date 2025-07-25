9d9dc86a995c121998e111c382e00ead76165280
Driya Govender <u23535793@tuks.co.za>
2025-07-25 15:44:46 +0200

    minimal sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-  return <Landing />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Box, Grid, Container } from '@mui/material';
 import Logo from "../media/namedLogo.png"
 
 export default function Landing() {
-    return (
-        <Box sx={{ backgroundColor: 'background.default', minHeight: '100vh' }}>
+  return (
+    <Box sx={{ backgroundColor: 'background.default', minHeight: '100vh' }}>
 
-        {/* navbar */}
-        <AppBar position="static" elevation={0} sx={{ backgroundColor: 'background.default' }}>
-            <Toolbar>
-                <Box component="img" src={Logo} alt="hierarKEY Logo" sx={{ height: 60, width: 150, ml: 4 }}/>
-                <Box sx={{ flexGrow: 1 }} />
-                <Button color="primary" varient="text" sx={{fontWeight: 'bold' }}>Login</Button>
-                <Button variant="contained" sx={{ ml: 2, mr: 6, backgroundColor: 'primary.main', color: 'white', fontWeight: 'bold', borderColor: 'white' }}>Sign Up</Button>
-            </Toolbar>
-        </AppBar>
+    {/* navbar */}
+    <AppBar position="static" elevation={0} sx={{ backgroundColor: 'background.default' }}>
+      <Toolbar>
+        <Box component="img" src={Logo} alt="hierarKEY Logo" sx={{ height: 60, width: 150, ml: 4 }}/>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button color="primary" varient="text" sx={{fontWeight: 'bold' }}>Login</Button>
+        <Link to="/signup" style={{ textDecoration: 'none' }}>
+          <Button variant="contained" sx={{ ml: 2, mr: 6, backgroundColor: 'primary.main', color: 'white', fontWeight: 'bold', borderColor: 'white' }}>Sign Up</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
 
       <Container sx={{ py: 10 }}>
         <Grid container spacing={4} alignItems="center">
